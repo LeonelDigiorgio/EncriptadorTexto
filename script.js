323c5beb942b469ab3b7586f0bca921a4ade021e
Leonel Di giorgio <leoneldigiorgio10@gmail.com>
2023-01-19 01:10:21 -0300

    cree evento para boton encriptar

---
+++ script.js
@@ -0,0 +1,16 @@
+let btnEncriptar = document.getElementById("btn-encriptar")
+btnEncriptar.addEventListener("click",encriptar)
+
+function encriptar() {
+    let textIngrese = document.getElementById("text-area")
+
+    if (textIngrese.value == "a") {
+        console.log("a");
+    }else{
+        console.log("otra");
+    }
+}
+
+
+
+
